deed0f561dcc53ef29898875ae0e7aa284e258ae
George Lesica <george@lesica.com>
2016-05-17 01:15:52 -0600

    Finish out cypher api spike.

--- src/neo4j.jl
+++ src/neo4j.jl
@@ -1,17 +1,17 @@
 # Basic Neo4j database driver that uses the HTTP Cypher API.
 
-import Requests: get, post, json
+import Requests: get, post, delete, json
 
-export Conn, defaultconn, Statement, Query, submit
+export Connection, defaultconn, Statement, Query, start, rollback, execute, commit, submit, cypher
 
 const PATH = "/db/data/"
 const PORT = 7474
 
 # -----------
-# Connections
+# Connection
 # -----------
 
-immutable Conn
+immutable Connection
   tls::Bool
   host::AbstractString
   port::Int
@@ -20,25 +20,25 @@ immutable Conn
   password::AbstractString
 end
 
-Conn(tls::Bool, host, port::Int, path) = Conn(tls, host, port, path, "", "")
+Connection(tls::Bool, host, port::Int, path) = Connection(tls, host, port, path, "", "")
 
-Conn(tls::Bool, host, user, pass) = Conn(tls, host, PORT, PATH, user, pass)
+Connection(tls::Bool, host, user, pass) = Connection(tls, host, PORT, PATH, user, pass)
 
-Conn(tls::Bool, host) = Conn(tls, host, PORT, PATH, "", "")
+Connection(tls::Bool, host) = Connection(tls, host, PORT, PATH, "", "")
 
-const defaultconn = Conn(false, "localhost", PORT, PATH)
+const defaultconn = Connection(false, "localhost", PORT, PATH)
 
-function connurl(c::Conn)
+function connurl(c::Connection)
   proto = ifelse(c.tls, "https", "http")
   "$(proto)://$(c.host):$(c.port)$(c.path)"
 end
 
-function connurl(c::Conn, suffix::AbstractString)
+function connurl(c::Connection, suffix::AbstractString)
   url = connurl(c)
   "$(url)$(suffix)"
 end
 
-function connheaders(c::Conn)
+function connheaders(c::Connection)
   headers = Dict(
     "Accept" => "application/json; charset=UTF-8",
     "Host" => "$(c.host):$(c.port)")
@@ -66,12 +66,84 @@ function cypher(stmt::AbstractString, params::Pair...)
   Statement(stmt, Dict(params))
 end
 
-function submit(c::Conn, query::Query)
-  resp = post(connurl(c, "transaction/commit");
-      headers=connheaders(c),
-      json=query)
-  json(resp)
+immutable Results
+  results::Vector
+  errors::Vector
 end
 
-submit(c::Conn, stmt::Statement) = submit(c, Query([stmt]))
+Results(raw::Dict) = Results(raw["results"], raw["errors"])
+
+immutable Transaction
+  connection::Connection
+  commit::AbstractString
+  location::AbstractString
+  results::Results
+end
+
+function start(c::Connection, q::Query)
+  resp = post(connurl(c, "transaction");
+      headers=connheaders(c),
+      json=q)
+  if resp.status != 201
+    warn("Failed to start transaction: $(resp.status)")
+  end
+  raw = json(resp)
+  Transaction(c, raw["commit"], resp.headers["Location"], Results(raw))
+end
+
+start(c::Connection, s::Statement) = start(c, Query([s]))
+
+function start(c::Connection)
+  q = Query([])
+  start(c, q)
+end
+
+function execute(t::Transaction, q::Query)
+  resp = post(t.location;
+      headers=connheaders(t.connection),
+      json=q)
+  if resp.status != 200
+    warn("Failed to continue transaction: $(resp.status)")
+  end
+  raw = json(resp)
+  Transaction(c, raw["commit"], t.location, Results(raw))
+end
+
+execute(t::Transaction, s::Statement) = execute(t, Query([s]))
+
+function commit(t::Transaction, q::Query)
+  resp = post(t.location;
+      headers=connheaders(t.connection),
+      json=q)
+  if resp.status != 200
+    warn("Failed to commit transaction: $(resp.status)")
+  end
+  raw = json(resp)
+  Results(raw)
+end
+
+commit(t::Transaction, s::Statement) = commit(t, Query([s]))
+commit(t::Transaction) = commit(t, Query([]))
+
+function rollback(t::Transaction)
+  resp = delete(t.location; headers=connheaders(t.connection))
+  if resp.status != 200
+    warn("Failed to rollback transaction: $(resp.status)")
+  end
+  raw = json(resp)
+  Results(raw)
+end
+
+function submit(c::Connection, q::Query)
+  resp = post(connurl(c, "transaction/commit");
+      headers=connheaders(c),
+      json=q)
+  if resp.status != 200
+    warn("Request failed: $(resp.status)")
+  end
+  raw = json(resp)
+  Request(raw)
+end
+
+submit(c::Connection, s::Statement) = submit(c, Query([s]))
 
